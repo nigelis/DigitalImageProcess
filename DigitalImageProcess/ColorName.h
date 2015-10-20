f527ceb87af868662fd6b4f5f02cc0a57f503dd9
Nigel Li <nigel.li@qq.com>
2015-10-20 14:27:00 +0800

    Convert Image to Color (via maximum probability)

---
+++ DigitalImageProcess/ColorName.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int ConvertImage2ColorNames(vector<int>& colorNames, InputArray src, OutputArray dst);
+int ConvertImage2Color(vector<int>& colorNames, InputArray src, OutputArray dst);
+
+
+int ReadConfigs(string filePath, vector<int>& colorNames);
+int ReadConfigs(string filePath, vector<vector<double>>& colorIndex);
